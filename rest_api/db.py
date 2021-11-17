5a3782adc0e7b647535a42a6524813e6dd2803f0
Carlos Bailon <cbailon@ugr.es>
2021-11-17 21:32:08 +0100

    Updated survey management

--- rest_api/db.py
+++ rest_api/db.py
@@ -1,10 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+import pytz
 
 from sqlalchemy import create_engine, extract
 from sqlalchemy.orm import sessionmaker
 
 from credentials import db_string
 from models.CompletedSurveys import CompletedSurveys
+from models.OngoingSurveys import OngoingSurveys
 from models.ParticipantDevice import ParticipantDevice
 from models.ParticipantStudy import ParticipantStudy
 from models.Study import Study
@@ -12,6 +14,7 @@ from models.StudySurveys import StudySurveys
 
 db = create_engine(db_string)
 Session = sessionmaker(bind=db)
+tz = pytz.timezone('Europe/Madrid')
 
 
 def get_study_id(participant_code):
@@ -33,7 +36,7 @@ def register_device(participant_code, device_id):
         session.commit()
 
 
-def _answered_today(survey, participant_code, hour):
+def _survey_answered(survey, participant_code, hour):
     with Session() as session:
         last_answer = session.query(CompletedSurveys).filter(CompletedSurveys.survey_id == survey.survey_id,
                                                                 CompletedSurveys.study_code == survey.study_code,
@@ -47,22 +50,48 @@ def _answered_today(survey, participant_code, hour):
         else:
             return False
 
+def _survey_ongoing(participant_code, hour):
+    with Session() as session:
+        last_record = session.query(OngoingSurveys).filter(OngoingSurveys.participant_code == participant_code,
+                                                            extract('year', OngoingSurveys.date) == datetime.now().year,
+                                                            extract('month', OngoingSurveys.date) == datetime.now().month,
+                                                            extract('day', OngoingSurveys.date) == datetime.now().day
+                                                            ).order_by(OngoingSurveys.date.desc()).first()
+        
+        if last_record:
+            if last_record.date.hour >= hour:
+                return last_record.date
+            else:
+                return False
+        else:
+            return False
 
 def get_survey_id(study_code, participant_code, hour, weekday):
     with Session() as session:
+        # Query surveys table
         surveys = session.query(StudySurveys).filter(StudySurveys.hours.any(hour),
                                                     StudySurveys.weekdays.any(weekday),
                                                     StudySurveys.study_code == study_code).all()
         for survey in surveys:
-            if survey and not _answered_today(survey=survey, participant_code=participant_code, hour=hour):
-                return survey.survey_id
+            # if survey time, continue - otherwise return None
+            if survey:
+                # if not answered, continue - otherwise return None
+                if not _survey_answered(survey=survey, participant_code=participant_code, hour=hour):
+                    # if not showing recently, send code - otherwise continue
+                    last_record = _survey_ongoing(participant_code=participant_code, hour=hour)
+                    if not last_record:
+                        return survey.survey_id
+                    else:
+                        # if showed less than 5 min ago, send code - otherwise return None
+                        timeframe_over = ((last_record + timedelta(minutes=5)) < tz.localize(datetime.now()))
+                        if timeframe_over:
+                            return survey.survey_id
 
         return None
 
 
 def register_completed_survey(study_code, participant_code, survey_id):
     with Session() as session:
-        registry = CompletedSurveys(study_code=study_code, participant_code=participant_code,
-                                    survey_id=survey_id, date=datetime.utcnow())
+        registry = OngoingSurveys(participant_code=participant_code, date=datetime.utcnow())
         session.add(registry)
         session.commit()
